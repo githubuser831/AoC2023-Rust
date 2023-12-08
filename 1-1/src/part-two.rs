314e63319f9fa6dcf3fa3be227cf2f44dd09df88
githubuser831 <150167998+githubuser831@users.noreply.github.com>
2023-12-08 16:41:35 -0600

    Update part-two.rs

--- 1-1/src/part-two.rs
+++ 1-1/src/part-two.rs
@@ -6,6 +6,23 @@ fn main() -> std::io::Result<()> {
     let mut file = File::open("testcase/example.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    assert_eq!(contents, "Hello, world!");
+    Solution::sanitize(&contents);
+    Solution::evaluate(&contents);
     Ok(())
+}
+
+struct Solution;
+
+impl Solution {
+    fn sanitize(contents: &String) -> String {
+        let sa = Regex::new("[0-9]").unwrap();
+        let nu = sa.replace_all(&contents, "");
+        print!("{}", nu);
+        // meant to sanitize only numbers
+    }
+    fn evaluate(contents: &String) -> String {
+        let ab = Regex::new("zero|one|two|three|four|five|six|seven|eight|nine").unwrap();
+        let tempchars: Vec<char> = contents.collect().reverse();
+        let ba = Regex::new("orez|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|").unwrap();
+    }
 }
