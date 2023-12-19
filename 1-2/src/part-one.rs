90c40732ca1a277429a9132432d526909075a034
githubuser831 <150167998+githubuser831@users.noreply.github.com>
2023-12-18 23:31:07 -0600

    Update part-one.rs

--- 1-2/src/part-one.rs
+++ 1-2/src/part-one.rs
@@ -1,36 +1,51 @@
 use std::fs::File;
 use std::io::prelude::*;
+use substring::Substring;
 
 fn main() -> std::io::Result<()> {
     let mut file = File::open("testcase/example.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     // How the data is extrapolated into usable strings.
-    let sum = Solution::ValidSum(&contents);
-    
-    println!("{}", sum);
-  
+    // let sum = Solution::ValidSum(&contents);
+    assert_eq!("foobar".to_string().substring(1, 6), "oobar");
+    //println!("{}", sum);
+
     Ok(())
 }
-
+/*
 struct Solution;
 
 impl Solution {
     fn ValidSum(contents: &String) -> i32 {
+        let r = 'red'
+        let g = 'green'
+        let b = 'blue'
+    
+        print('Substring count =', s.count('a'))
+    
+        s = 'This Is The Best Theorem'
+        print('Substring count =', s.count('Th'))
         let mut sum = 0;
         let lines = contents.lines();
-
+        // 12 red cubes
+        // 13 green cubes
+        // 14 blue cubes
+        assert_eq!("foobar".to_string().substring(1,6), "oobar");
+        /*
         for line in lines {
             println!("{}", line);
+            //
             if {
-                // check for invalids
+                // remove
                 continue;
-            }else {
+            } else {
                 // add game number to sum
             }
         }
-        
+
         return sum
-        
+        */
     }
 }
+*/
